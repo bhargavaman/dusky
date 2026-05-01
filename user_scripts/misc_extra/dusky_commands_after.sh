ee717609a4ef25fdfe3610b0728587476e4d2d12
dusk <dusk1@myyahoo.com>
2026-05-01 16:34:36 +0530

    dusky quickpanal

--- user_scripts/misc_extra/dusky_commands_after.sh
+++ user_scripts/misc_extra/dusky_commands_after.sh
@@ -31,6 +31,7 @@ declare -ra FLEET_COMMANDS=(
     "U | hyprctl reload"
     "U | systemctl --user enable --now mako.service || true"
     "S | systemctl enable --now ufw.service || true"
+    "U | systemctl --user enable --now dusky_quickpanal.service || true"
 )
 
 # ==============================================================================
