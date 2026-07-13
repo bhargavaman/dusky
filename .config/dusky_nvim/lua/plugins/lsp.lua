9e1ca2a5644b16fabb4c0b4dfceae9203b2b4c78
dusk <dusklinux@users.noreply.github.com>
2026-07-13 22:16:10 +0530

    turned of lsp

--- .config/dusky_nvim/lua/plugins/lsp.lua
+++ .config/dusky_nvim/lua/plugins/lsp.lua
@@ -75,7 +75,7 @@ return {
               },
               workspace = {
                 checkThirdParty = false,
-                library = vim.api.nvim_get_runtime_file("", true),
+                -- library = vim.api.nvim_get_runtime_file("", true),
               },
               telemetry = { enable = false },
             },
