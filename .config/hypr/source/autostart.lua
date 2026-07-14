b5ec57bccbf5ccb84e772f5bca12dcb75b14e365
dusk <dusklinux@users.noreply.github.com>
2026-07-14 11:24:41 +0530

    clip history fix

--- .config/hypr/source/autostart.lua
+++ .config/hypr/source/autostart.lua
@@ -29,8 +29,8 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("wl-clip-persist --clipboard regular --write-timeout 8000 --selection-size-limit 104857600 --reconnect-tries 0 --reconnect-delay 100")
 
     -- 2. Watchers after 0.8s, only store when there is real data to avoid re-entrancy / empty updates
-    hl.exec_cmd([[sh -c 'sleep 0.8; . $HOME/.config/dusky/settings/cliphist_db_env && exec wl-paste --type text --watch sh -c "[ \"$CLIPBOARD_STATE\" = data ] && cliphist store"' ]])
-    hl.exec_cmd([[sh -c 'sleep 0.8; . $HOME/.config/dusky/settings/cliphist_db_env && exec wl-paste --type image --watch sh -c "[ \"$CLIPBOARD_STATE\" = data ] && cliphist store"' ]])
+    hl.exec_cmd([[sh -c 'sleep 0.8; . $HOME/.config/dusky/settings/cliphist_db_env && exec wl-paste --type text --watch sh -c "[ \"\$CLIPBOARD_STATE\" = data ] && cliphist store"' ]])
+    hl.exec_cmd([[sh -c 'sleep 0.8; . $HOME/.config/dusky/settings/cliphist_db_env && exec wl-paste --type image --watch sh -c "[ \"\$CLIPBOARD_STATE\" = data ] && cliphist store"' ]])
 
 
 end)
